41f473724ac3bd9b4158f54a152aff938604bb29
Josh Seligman <joshua.seligman1@marist.edu>
2022-07-05 14:06:44 -0400

    Added basic memory operations

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"github.com/joshuaseligman/GoVM/pkg/hardware/memory"
+	"github.com/joshuaseligman/GoVM/pkg/util"
 )
 
 func main() {
 	ram := memory.NewMemory(0x10000)
-	ram.Log("Hello memory")
+	ram.SetMar(0x0)
+	ram.SetMdr(0x0000000000000005)
+	ram.Write()
+	ram.SetMar(0x10000)
+	ram.SetMdr(0x0)
+	ram.SetMar(0x0)
+	ram.Read()
+	ram.Log(util.ConvertToHex(ram.GetMdr(), 16))
 }
--- pkg/hardware/memory/memory.go
+++ pkg/hardware/memory/memory.go
@@ -6,12 +6,49 @@ import "github.com/joshuaseligman/GoVM/pkg/hardware"
 type Memory struct {
 	hw *hardware.Hardware
 	ram []int64
+	mar int
+	mdr int64
 }
 
 // Creates a new memory struct
 func NewMemory(addressableSpace int) *Memory {
-	mem := Memory { hw: hardware.NewHardware("RAM", 0), ram: make([]int64, addressableSpace)}
+	mem := Memory { 
+		hw: hardware.NewHardware("RAM", 0), 
+		ram: make([]int64, addressableSpace), 
+		mar: 0,
+		mdr: 0,
+	}
 	return &mem
+}
+
+// Sets the MDR to the value stored in memory at the address MAR
+func (mem *Memory) Read() {
+	mem.mdr = mem.ram[mem.mar] 
+}
+
+// Writes to RAM based on the current values of MAR and MDR
+func (mem *Memory) Write() {
+	mem.ram[mem.mar] = mem.mdr
+}
+
+// Gets the MAR
+func (mem *Memory) GetMar() int {
+	return mem.mar
+}
+
+// Gets the MDR
+func (mem *Memory) GetMdr() int64 {
+	return mem.mdr
+}
+
+// Sets the MAR
+func (mem *Memory) SetMar(newMar int) {
+	mem.mar = newMar
+}
+
+// Sets the MDR
+func (mem *Memory) SetMdr(newMdr int64) {
+	mem.mdr = newMdr
 }
 
 // Logs a message
