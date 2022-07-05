52c8f54f8f3e1ad6c16310204668c19dec97ddc3
Josh Seligman <joshua.seligman1@marist.edu>
2022-07-05 15:34:06 -0400

    Obtained the opcode and operands from the instruction

--- pkg/assembler/assembler.go
+++ pkg/assembler/assembler.go
@@ -4,11 +4,13 @@ import (
 	"os"
 	"log"
 	"bufio"
+	"strings"
 	"fmt"
 )
 
 // Assembles a program into instructions for the computer to read
 func AssembleProgram(filePath string, maxSize int) []uint32 {
+	// Open the file
 	f, err := os.Open(filePath)
 	if err != nil {
 		log.Fatal(err)
@@ -17,10 +19,24 @@ func AssembleProgram(filePath string, maxSize int) []uint32 {
 
 	scanner := bufio.NewScanner(f)
 
+	// Create the array that will become the memory
 	program := make([]uint32, maxSize)
 
+	// Read the file line by line
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		instr := scanner.Text()
+
+		// Get the end of the opcode
+		opcodeSplit := strings.Index(instr, " ")
+		if opcodeSplit == -1 {
+			log.Fatal("Invalid instruction ", instr)
+		}
+
+		opcode := instr[:opcodeSplit]
+		fmt.Println(opcode)
+
+		operands := strings.Split(instr[opcodeSplit + 1:], ", ")
+		fmt.Println(operands)
 	}
 	
 	return program
