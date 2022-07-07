e408d16fd2df3257e70f2e07283e412269a9d04a
Josh Seligman <joshua.seligman1@marist.edu>
2022-07-06 21:51:47 -0400

    Created the IDEX register

--- pkg/hardware/cpu/pipelineRegisters.go
+++ pkg/hardware/cpu/pipelineRegisters.go
@@ -24,3 +24,47 @@ func (ifidReg *IFIDReg) GetInstruction() uint32 {
 func (ifidReg *IFIDReg) GetIncrementedPC() uint {
 	return ifidReg.incrementedPC
 }
+
+// Struct for the data passed from the decode unit to the execute unit
+type IDEXReg struct {
+	instr uint32 // The fetched instruction
+	incrementedPC uint // The incremented program counter
+	regReadData1 uint64 // The data in the first read register
+	regReadData2 uint64 // The data in the second read register
+	signExtendImm uint64 // The sign extended immediate
+}
+
+// Function to create the IDEX register
+func NewIDEXReg(instruction uint32, pc uint, reg1 uint64, reg2, uint64, imm uint64) *IDEXReg {
+	reg := IDEXReg {
+		instr: instruction,
+		incrementedPC: pc,
+		regReadData1: reg1,
+		regReadData2: reg2,
+		signExtendImm: imm,
+	}
+	return &reg
+}
+
+// Gets the instruction in the IDEX register
+func (idexReg *IDEXReg) GetInstruction() uint32 {
+	return idexReg.instr
+}
+
+// Gets the incremented program counter in the IDEX register
+func (idexReg *IDEXReg) GetIncrementedPC() uint {
+	return idexReg.incrementedPC
+}
+
+// Gets the incremented program counter in the IDEX register
+func (idexReg *IDEXReg) GetRegReadData1() uint64 {
+	return idexReg.regReadData1
+}
+// Gets the incremented program counter in the IDEX register
+func (idexReg *IDEXReg) GetRegReadData2() uint64 {
+	return idexReg.regReadData2
+}
+// Gets the incremented program counter in the IDEX register
+func (idexReg *IDEXReg) GetSignExtendedImmediate() uint64 {
+	return idexReg.signExtendImm
+}
